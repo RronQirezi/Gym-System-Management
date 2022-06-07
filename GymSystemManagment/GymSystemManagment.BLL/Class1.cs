e1e596cce13b914d368e207641df5b9309c4e26b
Rron <rrqirezi@gmail.com>
2022-06-07 09:53:21 +0200

    sdfd

---
+++ GymSystemManagment/GymSystemManagment.BLL/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace GymSystemManagment.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ GymSystemManagment/GymSystemManagment.DAL/DatabaseConnection.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace GymSystemManagment.DAL
+{
+    public class DatabaseConnection
+    {
+    }
+}
